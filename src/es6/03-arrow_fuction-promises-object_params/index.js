acdcfefce98d19bdec32d0b9a65b1f6406638ea6
CabosManuel <cabos.apolaya@gmail.com>
2022-09-06 18:35:19 -0500

    Arrow function, Promises and Object params

---
+++ src/es6/03-arrow_fuction-promises-object_params/index.js
@@ -0,0 +1,48 @@
+// Objects params --------------------------
+let firstName = 'Manuel';
+let age = 22;
+
+obj = { firstName: firstName, age: age};
+
+//es6
+obj2 = {firstName, age};
+
+console.log(obj);
+console.log(obj2);
+
+// ARROW FUNCTION ---------------------------
+const names = [
+  {name: 'Manuel', age: 22},
+  {name: 'Oscar', age: 32}
+];
+
+let listOfNames = names.map(function (item) {
+  console.log(item.name);
+});
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (firstName, age, country) => {
+  // ...
+}
+
+const listOfNames4 = firstName => {
+  // ...
+}
+
+const square = num => num * num;
+console.log(square(12)); // 144
+
+// PROMISES ---------------------------------
+const helloPromise = () => {
+  return new Promise( (resolve, reject) => {
+
+    if(true) resolve('Succsess!');
+    else reject('Wrong!');
+
+  });
+}
+
+helloPromise()
+  .then(response => console.log(response))
+  .catch(error => console.log(error));
