004d1ee987428f4c0f109811e01431fe248da669
CabosManuel <cabos.apolaya@gmail.com>
2022-09-06 09:46:48 -0500

    LET, CONST, Multiline, Spread Operator and Destructuring

---
+++ src/es6/02-let_const-multiline-spread_operator-destructuring/index.js
@@ -0,0 +1,53 @@
+// let y const -----------------------------
+// let
+var hola = 'hola';
+
+{
+  var globalVar = 'Global var';
+}
+
+{
+  let globalLet = 'Global let';
+  console.log(globalLet);
+}
+
+console.log(globalVar);
+
+//const
+const a = 'b';
+//a = 'a';
+console.log(a);
+
+// Multiline ---------------------------------------
+let lorem = 'Antes de ES6 se utilizaba "\\n" \n' +
+            'para tener multilineas'
+
+// es6
+let lorem2 = 
+`Ahora con ES6, utilizando las comillas francesas (\`)
+y dando saltos de línea ya no hace falta
+usar el \\n`
+
+console.log(lorem);
+console.log(lorem2);
+
+// Destructuring ----------------------------------
+let person = {
+  'firstName' : 'Manuel',
+  'age': 22,
+  'country': 'PE'
+};
+
+console.log(person.firstName, person.age, person.country);
+
+// es6
+let {firstName, age, country} = person;
+console.log(firstName, age, country);
+
+
+// Spread Operator (Operador de propagación) ------------
+let team1 = ['Manuel', 'Alejandro', 'Oscar'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+console.log(education);
