7d76b1e0ac224cf4bc4215b1872bce80330625d7
Manuel Cabos <cabos.apolaya@gmail.com>
2022-09-07 01:08:05 -0500

    Classes, Modules and Generator

---
+++ src/es6/04-classes-modules-generators/index.js
@@ -0,0 +1,35 @@
+// Classes ---------------------------------
+class Calculator {
+  constructor () {
+    this.valueA = 0;
+    this.valueB = 0;
+  }
+
+  sum(valueA, valueB) {
+    this.valueA = valueA;
+    this.valueB = valueB;
+
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new Calculator();
+console.log(calc.sum(2, 2));
+
+// Modules -----------------------------
+import { hello } from './module'
+hello();
+
+// Generators --------------------------
+function* helloWorld() {
+  if (true) 
+    yield 'Hello, ';
+
+  if (true)
+    yield 'World!'
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
