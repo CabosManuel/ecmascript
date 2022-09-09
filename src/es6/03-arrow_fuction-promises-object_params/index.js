73465dc484ccd9f1023c07f3f3962d8719852f55
Manuel Cabos <cabos.apolaya@gmail.com>
2022-09-08 22:06:42 -0500

    More examples of Arrow fuctions

--- src/es6/03-arrow_fuction-promises-object_params/index.js
+++ src/es6/03-arrow_fuction-promises-object_params/index.js
@@ -30,6 +30,15 @@ const listOfNames4 = firstName => {
   // ...
 }
 
+// examples
+function square(num) {
+  return num * num;
+}
+
+const square = (num) => {
+  return num * num;
+}
+
 const square = num => num * num;
 console.log(square(12)); // 144
 
