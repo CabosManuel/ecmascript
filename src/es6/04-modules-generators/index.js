b5fa4a9f69d20827763f1d37d8f7761305a15b76
CabosManuel <cabos.apolaya@gmail.com>
2022-10-20 00:17:28 -0500

    Organize file of Classes

--- src/es6/04-modules-generators/index.js
+++ src/es6/04-modules-generators/index.js
@@ -1,21 +1,3 @@
-// Classes ---------------------------------
-class Calculator {
-  constructor () {
-    this.valueA = 0;
-    this.valueB = 0;
-  }
-
-  sum(valueA, valueB) {
-    this.valueA = valueA;
-    this.valueB = valueB;
-
-    return this.valueA + this.valueB;
-  }
-}
-
-const calc = new Calculator();
-console.log(calc.sum(2, 2));
-
 // Modules -----------------------------
 import { hello } from './module'
 hello();
