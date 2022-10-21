a498d969a3a419aaa52808a011e2da275a3181f6
CabosManuel <cabos.apolaya@gmail.com>
2022-10-20 22:21:50 -0500

    Exponential operator and Array includes

---
+++ src/es7/02-array-includes/array-includes.js
@@ -0,0 +1,6 @@
+let numbers = [1, 3, 4, 2, 5];
+console.log(numbers.includes(4)); // true
+
+const names = ['Manuel', 'Alejandro', 'Oscar'];
+console.log(names.includes('Oscar')); // true
+console.log(names.includes('oscar')); // false
