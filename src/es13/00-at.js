1c3d078b7490e88898b00eb6a76915e1c2b437a3
CabosManuel <cabos.apolaya@gmail.com>
2022-11-01 11:56:05 -0500

    Update at()

--- src/es13/00-at.js
+++ src/es13/00-at.js
@@ -1,4 +1,8 @@
-const array = ['ichi','ni','san','shi','go','roku','nana','hachi','kyu'];
+const array = ['ichi','ni','san','shi','go','roku'];
+// positivo      1,     2,   3,    4,   5,  length
+// negativo   -length, -5,  -4,   -3,  -2,   -1
 
 console.log(array[array.length-1]); // kyu
+console.log(array[-1]); // undefined
+console.log(array.at(-4)); // san
 console.log(array.at(-1)); // kyu
